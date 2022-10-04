9ad33bebd97193627220279dc85ef600ed338e18
Shriti <shriti2018@gmail.com>
2022-10-04 12:53:27 +0530

    Modify Strings in Python

---
+++ modifystrings.py
@@ -0,0 +1,21 @@
+#UPPER-CASE
+a="I am a Developer."
+print(a.upper())        #upper() returns uppercase values
+
+#LOWER-CASE
+a="I am a Developer"
+print(a.lower())        #lower() returns lowercase values
+
+#REMOVES WHITESPACES 
+a="   Hello World  "
+print(a.strip())        #strip() removes whitespaces from the beginning or end
+
+#REPLACE STRING
+
+a="hello world"
+print(a.replace('h','H'))   #replace string with another string
+
+#SPLIT STRINGS
+a="Hello,World"
+print(a.split(','))
+
